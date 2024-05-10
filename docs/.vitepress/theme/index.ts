bc8caa8ef4cdf9208ff93918c29468b53e981ff6
RJY <junyeren@outlook.com>
2024-05-10 22:33:48 +0800

    feat(all):giscus评论功能实现

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -36,6 +36,9 @@ import mediumZoom from 'medium-zoom';
 import { onMounted, watch, nextTick } from 'vue';
 import { useRoute } from 'vitepress';
 import './style/index.css'
+//评论giscus
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData } from 'vitepress';
 
 export const Theme: ThemeConfig = {
   extends: DefaultTheme,
@@ -85,6 +88,28 @@ export const Theme: ThemeConfig = {
     watch(
       () => route.path,
       () => nextTick(() => initZoom())
+    );
+    //giscus评论插件
+    // Get frontmatter and route
+    const { frontmatter } = useData();
+        
+    // giscus配置
+    giscusTalk({
+      repo: 'get1024/RyanJoy-s_Web', //仓库
+      repoId: 'R_kgDOL1mSLA', //仓库ID
+      category: 'Announcements', // 讨论分类
+      categoryId: 'DIC_kwDOL1mSLM4CfRSC', //讨论分类ID
+      mapping: 'pathname',
+      inputPosition: 'bottom',
+      lang: 'zh-CN',
+      }, 
+      {
+        frontmatter, route
+      },
+      //默认值为true，表示已启用，此参数可以忽略；
+      //如果为false，则表示未启用
+      //您可以使用“comment:true”序言在页面上单独启用它
+      true
     );
   },
 }
