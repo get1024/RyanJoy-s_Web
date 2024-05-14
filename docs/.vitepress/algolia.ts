f671e7efdd05a8946ba8337bd02afd43711c5541
RJY <junyeren@outlook.com>
2024-05-14 10:32:24 +0800

    feat(system):algolia search

--- docs/.vitepress/algolia.ts
+++ docs/.vitepress/algolia.ts
@@ -1,7 +1,7 @@
 export default {
-  appId: 'U291LNG6OV',
-  apiKey: 'c0453ea63d083510e34481b4d101718e',
-  indexName: 'RyanJoy',
+  appId: "VCE8UETSXA",
+  apiKey: "c4555838d125590a4e56ae03ca580905",
+  indexName: 'get1024io',
   placeholder: '请输入关键词',
   translations: {
     button: {
