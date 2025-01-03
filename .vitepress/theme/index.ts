528abe400d35080878d596fce2603cdbc03358e7
RJY <junyeren@outlook.com>
2025-01-04 00:06:20 +0800

    perf(config):delete image zoomer

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -121,17 +121,17 @@ export const Theme: ThemeConfig = {
   //图片缩放
   setup() {
     const route = useRoute();
-    const initZoom = () => {
-      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
-      mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
-    };
-    onMounted(() => {
-      initZoom();
-    });
-    watch(
-      () => route.path,
-      () => nextTick(() => initZoom())
-    );
+    // const initZoom = () => {
+    //   // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
+    //   mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+    // };
+    // onMounted(() => {
+    //   initZoom();
+    // });
+    // watch(
+    //   () => route.path,
+    //   () => nextTick(() => initZoom())
+    // );
     //giscus评论插件
     // Get frontmatter and route
     const { frontmatter } = useData();
