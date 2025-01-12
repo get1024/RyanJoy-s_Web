7e78a9c334aaaeca6a269c2b54c29d1302dfddfc
RJY <junyeren@outlook.com>
2025-01-12 19:53:43 +0800

    feat(noteIndex):components optimize

--- .vitepress/theme/components/noteIndex.vue
+++ .vitepress/theme/components/noteIndex.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { data as posts } from '../utils/note.data.ts'
 import '../style/nodeIndex.css'
+import { useData } from "vitepress"
 
 
 const groupedPosts = Object.entries(posts)
@@ -32,13 +33,16 @@ const groupedPosts = Object.entries(posts)
 
 <template>
   <div v-for="group in groupedPosts" :key="group.year">
-    <h2>{{ group.year }}年</h2>
-    <ul>
+    <h2 class="noteIndex-h2">{{ group.year }}年</h2>
+    <ul class="noteIndex-ul">
       <li v-for="post of group.posts" :key="post.url" class="post-item">
-        <a :href="'/RyanJoy-s_Web'+post.url">{{ post.frontmatter.title }}</a>
+        <span class="post-link-update">
+          <a :href="'/RyanJoy-s_Web'+post.url">{{ post.frontmatter.title }}</a>
+          &nbsp;
+          <span class="post-update">更新于{{ post.frontmatter.updateAt.year }}-{{ post.frontmatter.updateAt.month }}-{{ post.frontmatter.updateAt.day }} {{ post.frontmatter.updateAt.hour }}:{{ post.frontmatter.updateAt.minute }}:{{ post.frontmatter.updateAt.second }}</span>
+        </span>
         <span class="post-date">创建于 {{ post.frontmatter.createAt.year }}-{{ post.frontmatter.createAt.month }}-{{ post.frontmatter.createAt.day }} {{ post.frontmatter.createAt.hour }}:{{ post.frontmatter.createAt.minute }}:{{ post.frontmatter.createAt.second }}</span>
       </li>
     </ul>
   </div>
 </template>
-
