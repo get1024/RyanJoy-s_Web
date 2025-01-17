32cf9823ed93f207a706d4e1c3edebc87589bd77
RJY <junyeren@outlook.com>
2025-01-18 00:38:31 +0800

    new try.......fuck!

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
     site: 'https://get1024.github.io',
     base: '/RyanJoy-s_Web',
+    build: {
+        outDir: '.vitepress/dist' // 设置构建输出目录
+    }
 })
