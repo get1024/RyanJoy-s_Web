accb9ea7e74c559a05a42334a7d15a5878f34855
RJY <junyeren@outlook.com>
2025-01-20 11:59:52 +0800

    feat(sitemap):add sitemap.xml to website

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 //vite.config.ts
 
 import { defineConfig } from "vite";
+import Sitemap from 'vite-plugin-sitemap';
+import Vue from '@vitejs/plugin-vue'; 
 
 export default defineConfig({
-    
+    plugins:[
+        Vue(),
+        Sitemap({
+            hostname: 'https://get1024.github.io/RyanJoy-s_Web/',
+            changefreq: 'daily',
+        }),
+    ]
 });
