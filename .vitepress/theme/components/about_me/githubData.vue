083013dfac571b9a7df32207d65a8200d24ab410
RyanJoy <ryanjoy2002@gmail.com>
2025-02-05 10:26:16 +0800

    test fail

--- .vitepress/theme/components/about_me/githubData.vue
+++ .vitepress/theme/components/about_me/githubData.vue
@@ -1,11 +1,11 @@
 <template>
     <div align="center">
         <img
-    src="https://blog.ryanjoy.top/api/proxy/api?username=get1024&show_icons=true&role=OWNER,ORGANIZATION_MEMBER,COLLABORATOR&hide_border=true&show_owner=true" />
+            alt="本图片需要开启代理才能访问" src="https://github-readme-stats.vercel.app/api?username=get1024&show_icons=true&role=OWNER,ORGANIZATION_MEMBER,COLLABORATOR&hide_border=true&show_owner=true" />
     </div>
 
     <div align="center">
         <img
-    src="https://blog.ryanjoy.top/api/proxy/graph?username=get1024&bg_color=fafafa&color=9e4c98&line=ffae00&point=d45a25&area=true&hide_border=true" />
+        alt="本图片需要开启代理才能访问" src="https://github-readme-activity-graph.vercel.app/graph?username=get1024&bg_color=fafafa&color=9e4c98&line=ffae00&point=d45a25&area=true&hide_border=true" />
     </div>
 </template>
